c88b2f768625ae89765cc4b0fa53d324aaa6f1b7
Stefan de Konink <stefan@konink.de>
2013-08-23 03:32:40 +0200

    Add the tripUpdate protobuf files for the dynamic tests.
    Added the helperscript to generate them.

---
+++ scripts/gtfsrt/dynamic_tests_gtfsrt.py
@@ -0,0 +1,65 @@
+from gtfs_realtime_pb2 import FeedMessage, FeedHeader, TripUpdate, TripDescriptor
+
+feedmessage = FeedMessage()
+feedmessage.header.gtfs_realtime_version = "1.0"
+feedmessage.header.incrementality = FeedHeader.FULL_DATASET
+
+feedmessage.header.timestamp = 1388530800 # 2014-01-01
+
+feedentity = feedmessage.entity.add()
+feedentity.id = '3b'
+feedentity.trip_update.trip.start_date = '20140101'
+feedentity.trip_update.trip.trip_id = '3b|1'
+update = feedentity.trip_update.stop_time_update.add()
+update.stop_sequence = 0
+update.stop_id = '3b1'
+update.departure.delay = 600
+
+feedentity = feedmessage.entity.add()
+feedentity.id = '3b|2'
+feedentity.trip_update.trip.start_date = '20140101'
+feedentity.trip_update.trip.trip_id = '3b|2'
+update = feedentity.trip_update.stop_time_update.add()
+update.stop_sequence = 1
+update.stop_id = '3b1'
+update.departure.delay = 60
+
+f = open("/tmp/3b.pb", "wb")
+f.write(feedmessage.SerializeToString())
+f.close()
+
+feedmessage = FeedMessage()
+feedmessage.header.gtfs_realtime_version = "1.0"
+feedmessage.header.incrementality = FeedHeader.FULL_DATASET
+
+feedmessage.header.timestamp = 1388531040 # 2014-01-01 00:04
+
+feedentity = feedmessage.entity.add()
+feedentity.id = '3c'
+feedentity.trip_update.trip.start_date = '20140101'
+feedentity.trip_update.trip.trip_id = '3c|1'
+update = feedentity.trip_update.stop_time_update.add()
+update.stop_sequence = 2
+update.stop_id = '3c2'
+update.arrival.delay = 120
+
+f = open("/tmp/3c.pb", "wb")
+f.write(feedmessage.SerializeToString())
+f.close()
+
+feedmessage = FeedMessage()
+feedmessage.header.gtfs_realtime_version = "1.0"
+feedmessage.header.incrementality = FeedHeader.FULL_DATASET
+
+feedmessage.header.timestamp = 1388531040 # 2014-01-01 00:04
+
+feedentity = feedmessage.entity.add()
+feedentity.id = '3e'
+feedentity.trip_update.trip.start_date = '20140101'
+feedentity.trip_update.trip.trip_id = '3e|1'
+feedentity.trip_update.trip.schedule_relationship = TripDescriptor.CANCELED
+
+f = open("/tmp/3e.pb", "wb")
+f.write(feedmessage.SerializeToString())
+f.close()
+
