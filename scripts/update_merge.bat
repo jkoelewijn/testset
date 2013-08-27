e46bf63a1ecf7c30822022034e84f83fbba456f1
jkoelewijn <git@jkoelewijn.nl>
2013-08-27 14:33:08 +0200

    Update update_merge.bat to use the new structure of the testset

--- scripts/update_merge.bat
+++ scripts/update_merge.bat
@@ -1,28 +1,29 @@
-@ECHO OFF
+@echo off
 
-REM Set data directory with GTFS files as text in separate directories
-SET DATA_DIRECTORY="%~dp0..\data\gtfs"
+rem Set data directory with GTFS files as text in separate directories
+rem containing a gtfs folder
+set DATA_DIRECTORY="%~dp0..\tests"
 
-REM Set output directory for tests in GTFS zip format
-REM Don't chose a directory inside %DATA_DIRECTORY%
-SET GTFS_OUTPUT_DIRECTORY="%~dp0tests"
+rem Set output directory for tests in GTFS zip format
+rem Don't chose a directory inside %DATA_DIRECTORY%
+set GTFS_OUTPUT_DIRECTORY="%~dp0tests"
 
-REM Set output file for all tests combined in GTFS zip format
-SET ALL_TESTS_ZIP="%~dp0alltests.zip"
+rem Set output file for all tests combined in GTFS zip format
+set ALL_TESTS_ZIP="%~dp0alltests.zip"
 
 
-REM Add 7-zip path to PATH
-SET SEVEN_ZIP_PATH="C:\Program Files\7-Zip"
-IF EXIST %SEVEN_ZIP_PATH% SET PATH=%PATH%;%SEVEN_ZIP_PATH%
-SET SEVEN_ZIP_PATH="C:\Program Files (x86)\7-Zip"
-IF EXIST %SEVEN_ZIP_PATH% SET PATH=%PATH%;%SEVEN_ZIP_PATH%
+rem Add 7-zip path to PATH
+set SEVEN_ZIP_PATH="C:\Program Files\7-Zip"
+if exist %SEVEN_ZIP_PATH% set PATH=%PATH%;%SEVEN_ZIP_PATH%
+set SEVEN_ZIP_PATH="C:\Program Files (x86)\7-Zip"
+if exist %SEVEN_ZIP_PATH% set PATH=%PATH%;%SEVEN_ZIP_PATH%
 
-REM Create tests in GTFS zip format
-MKDIR %GTFS_OUTPUT_DIRECTORY%
-DEL /Q %GTFS_OUTPUT_DIRECTORY%\*.*
-FOR /F "delims=" %%f in ('dir %DATA_DIRECTORY% /A:D /B /O:N') DO (
-    7z a %GTFS_OUTPUT_DIRECTORY%\"%%f.zip" %DATA_DIRECTORY%\"%%f"\*.txt
+rem Create tests in GTFS zip format
+mkdir %GTFS_OUTPUT_DIRECTORY%
+del /Q %GTFS_OUTPUT_DIRECTORY%\*.*
+for /F "delims=" %%f in ('dir %DATA_DIRECTORY% /A:D /B /O:N') do (
+    7z a %GTFS_OUTPUT_DIRECTORY%\"%%f.zip" %DATA_DIRECTORY%\"%%f"\gtfs\*.txt
 )
 
-REM Combine tests
+rem Combine tests
 java -jar "%~dp0..\bin\onebusaway-gtfs-transformer-cli.jar" --overwriteDuplicates %GTFS_OUTPUT_DIRECTORY%\*.zip %ALL_TESTS_ZIP%
