dd6ab4c830fbfc32b1659da99de2e54bea352021
Jasper Hartong <jasper@calendar42.com>
2013-09-03 13:10:27 +0200

    added GTFSRT for 3i and added the whole plannerstack scenario

--- scripts/gtfsrt/dynamic_tests_gtfsrt.py
+++ scripts/gtfsrt/dynamic_tests_gtfsrt.py
@@ -63,3 +63,40 @@ f = open("/tmp/3e.pb", "wb")
 f.write(feedmessage.SerializeToString())
 f.close()
 
+
+feedmessage = FeedMessage()
+feedmessage.header.gtfs_realtime_version = "1.0"
+feedmessage.header.incrementality = FeedHeader.FULL_DATASET
+
+feedmessage.header.timestamp = 1388530800 # 2014-01-01
+
+feedentity = feedmessage.entity.add()
+feedentity.id = '3i'
+translation = feedentity.alert.header_text.translation.add()
+translation.text = 'Unknown effect'
+entityselector =  feedentity.alert.informed_entity.add()
+entityselector.route_id = '3i|1'
+
+f = open("/tmp/3i.pb", "wb")
+f.write(feedmessage.SerializeToString())
+f.close()
+
+
+feedmessage = FeedMessage()
+feedmessage.header.gtfs_realtime_version = "1.0"
+feedmessage.header.incrementality = FeedHeader.FULL_DATASET
+
+feedmessage.header.timestamp = 1388530800 # 2014-01-01
+
+feedentity = feedmessage.entity.add()
+feedentity.id = 'plannerstack_scenario'
+feedentity.trip_update.trip.start_date = '20140101'
+feedentity.trip_update.trip.trip_id = 'plannerstack_scenario|intercity'
+update = feedentity.trip_update.stop_time_update.add()
+update.stop_sequence = 1
+update.stop_id = 'plannerstack_scenario1'
+update.departure.delay = 600
+
+f = open("/tmp/plannerstack_scenario1.pb", "wb")
+f.write(feedmessage.SerializeToString())
+f.close()
